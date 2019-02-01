e5aaaf9b1b0640b53300ada1a49b8c6e92d6de45
zhujun <zhujundn@163.com>
2019-02-01 18:08:56 +0800

    add pics

--- mock/search/list.js
+++ mock/search/list.js
@@ -12,7 +12,7 @@ module.exports = {
             id: Math.random().toString().slice(2)
         },
         {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022145750123-1745839503.jpg',
+            img: path + '/search/138012-20161022145750123-1745839503.jpg',
             title: '漫漫火锅',
             subTitle: '[王府井]自助火锅',
             price: '113',
@@ -21,7 +21,7 @@ module.exports = {
             id: Math.random().toString().slice(2)
         },
         {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022145755545-1770557408.jpg',
+            img: path + '/search/138012-20161022145755545-1770557408.jpg',
             title: '北方涮肉',
             subTitle: '什刹海店',
             price: '92',
@@ -30,7 +30,7 @@ module.exports = {
             id: Math.random().toString().slice(2)
         },
         {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022145800732-576947550.jpg',
+            img: path + '/search/138012-20161022145800732-576947550.jpg',
             title: '姓高火锅',
             subTitle: '知春里店',
             price: '90',
@@ -39,30 +39,12 @@ module.exports = {
             id: Math.random().toString().slice(2)
         },
         {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022145806201-1193851669.jpg',
-            title: '八重牛府',
-            subTitle: '最好吃的牛丸',
-            price: '85',
-            distance: '160',
-            mumber: '1426',
-            id: Math.random().toString().slice(2)
-        },
-        {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022150855185-1659375763.jpg',
+            img: path + '/search/138012-20161022150855185-1659375763.jpg',
             title: '蜀乡涮锅',
             subTitle: '[王府井]自助火锅',
             price: '113',
             distance: '140m',
             mumber: '689',
-            id: Math.random().toString().slice(2)
-        },
-        {
-            img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161022145800732-576947550.jpg',
-            title: '满楼福火锅',
-            subTitle: '知春路店',
-            price: '90',
-            distance: '160',
-            mumber: '58',
             id: Math.random().toString().slice(2)
         }
     ]
